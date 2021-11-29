d1b797a2df58abcd8ce4ad3d62cabd4612994857
Anonymous <40665494+shfsakib@users.noreply.github.com>
2021-11-29 18:57:58 +0600

    login page created

---
+++ ASLPortfolioWebApp/ASLPortfolioWebApp/admin/log-in.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASLPortfolioWebApp.admin
+{
+    public partial class log_in : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_ServerClick(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void lnkForgot_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
